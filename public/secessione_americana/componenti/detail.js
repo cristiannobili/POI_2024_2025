d8f4ed700f4fe5afdaf2896af53f7fa5f4d82642
Nicolas Baez <baeznicolas@itis-molinari.eu>
2025-02-02 14:04:09 +0100

    Url

--- public/secessione_americana/componenti/detail.js
+++ public/secessione_americana/componenti/detail.js
@@ -62,60 +62,7 @@ export const createDetail = (parentElement) => {
         },
         navigateToDetail: (id) => {
             // Aggiorna l'URL con l'ID del posto
-            window.history.pushState({}, "", `#posto-${id}`);
-            
-            // Rendiamo visibile la pagina del posto
-            const page = document.getElementById("pagina_posto");
-            if (page) {
-                page.classList.remove("hidden");
-                page.classList.add("visible");
-                const home = document.getElementById("home");
-                home.classList.remove("visible");
-                home.classList.add("hidden");
-            }
-            
-            // Ora carichiamo i dettagli specifici del posto
-            const item = data.find(d => d.name.id === id);
-            console.log(data)
-            if (item) {
-                console.log(item)
-                const template = `
-                    <div class="container mt-5">
-                        <header class="mb-4">
-                            <div class="d-flex justify-content-between align-items-center">
-                                <h1 class="h3 mt-3">${item.name.Titolo}</h1>
-                                <a href="" class="btn btn-primary mt-3">Torna ad HOME</a>
-                            </div>
-                        </header>
-                        <div class="row">
-                            <div class="col-md-8">
-                                <section class="mb-4">
-                                    <h2 class="h5 mt-3">Descrizione</h2>
-                                    <p class="mt-3">${item.name.Paragrafo_1}</p>
-                                </section>
-                                <section class="mb-4">
-                                    <h2 class="h5 mt-3">Conseguenze</h2>
-                                    <p class="mt-3">${item.name.Paragrafo_2}</p>
-                                </section>
-                                <section class="mb-4">
-                                    <h2 class="h5 mt-3">Riflessioni</h2>
-                                    <p class="mt-3">${item.name.Paragrafo_3}</p>
-                                </section>
-                            </div>
-                            <div class="col-md-4">
-                                <div class="border rounded bg-light d-flex align-items-center justify-content-center height-auto">
-                                    <span class="text-center">
-                                        <img src="${item.name.Immagine_1}" alt="${item.name.Titolo}" class="immaginiDetail" />
-                                    </span>
-                                </div>
-                            </div>
-                        </div>
-                    </div>`;
-                
-                // Carica il template all'interno del div della pagina
-                console.log(template)
-                page.innerHTML = template;
-            }
+            window.history.pushState({}, "", `#posto-${id}`);        
         }
     };
     return self;
