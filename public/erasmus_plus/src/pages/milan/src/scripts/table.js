5eb255d6ff03772dbeaaa3b591f89a3aa1c24807
AngeloRipamonti <ripamontiangelo@itis-molinari.eu>
2025-02-02 11:23:15 +0100

    ErasmusPlus: Webfonts, start admin table, fix footer

--- public/erasmus_plus/src/pages/milan/src/scripts/table.js
+++ public/erasmus_plus/src/pages/milan/src/scripts/table.js
@@ -81,53 +81,45 @@ export const createAdminTable = (parentElement, pubsub) => {
             if (!data) throw new Error("No data to render");
             let listToShow = data.milan;
             let html = `
-                    <table class="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
-                    <caption class="sticky top-0"> List of all POI </caption>
-                                <thead
-                                    class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400 sticky top-0">
-                                    <tr>
-                                        <th scope="col" class="p-4 break-words whitespace-normal p-2">Title</th>
-                                        <th scope="col" class="p-4 break-words whitespace-normal p-2">Description</th>
-                                        <th scope="col" class="p-4 break-words whitespace-normal p-2">Address</th>
-                                        <th scope="col" class="p-4 break-words whitespace-normal p-2">Coords</th>
-                                        <th scope="col" class="p-4 break-words whitespace-normal p-2">Price</th>
-                                        <th scope="col" class="p-4 break-words whitespace-normal p-2">Photo</th>
-                                        <th scope="col" class="p-4 break-words whitespace-normal p-2">Edit</th>
-                                        <th scope="col" class="p-4 break-words whitespace-normal p-2">Remove</th>
-                                    </tr>
-                                </thead>
+                    <table class="table-responsive w-full rounded">
+                        <thead>
+                            <tr>
+                                <th class="border w-1/4 px-4 py-2">Title</th>
+                                <th class="border w-1/6 px-4 py-2">Description</th>
+                                <th class="border w-1/6 px-4 py-2">Address</th>
+                                <th class="border w-1/6 px-4 py-2">Coords</th>
+                                <th class="border w-1/6 px-4 py-2">Price</th>
+                                <th class="border w-1/7 px-4 py-2">Photo</th>
+                                <th class="border w-1/5 px-4 py-2">Actions</th>
+                            </tr>
+                        </thead>
                         <tbody>`;
+                        
             for (const element in listToShow) {
-                html += `<tr
-                            class="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600">
-                                <td class="px-6 py-4 break-words whitespace-normal p-2">`+ listToShow[element].name + `</td>
-                                <td class="px-6 py-4 text-left p-0 break-words whitespace-normal p-2">
-                                    <div class="max-h-[150px] overflow-y-auto p-2 text-justify">
-                                        `+ listToShow[element].description + `
-                                    </div>
-                                </td>
-                                <td class="px-6 py-4 break-words whitespace-normal p-2">`+ listToShow[element].adress + `</td>
-                                <td class="px-6 py-4 break-words whitespace-normal p-2">`+ parseFloat(listToShow[element].lat).toFixed(2) + ", " + parseFloat(listToShow[element].lon).toFixed(2) + `</td>
-                                <td class="px-6 py-4 break-words whitespace-normal p-2">`+ listToShow[element].price + `</td>
-                                <td class="px-6 py-4 break-words whitespace-normal p-2"><div>`;
-                listToShow[element].imageLink.forEach(img => {
-                    html += `<img src="` + img + `" class="rounded-lg duration-700 ease-in-out" data-carousel-item>`;
-                })
-                html += `</div></td>
-                                <td class="px-6 py-4 break-words whitespace-normal p-2"><button type="button" id="edit-`+ element + `"
-                                    class="font-medium text-blue-600 dark:text-blue-500 hover:underline">EDIT</button>
-                                </td>
-                                <td class="px-6 py-4 break-words whitespace-normal p-2">
+                html += `<tr>
+                            <td class="border px-4 py-2">${listToShow[element].name}</td>
+                            <td class="border px-4 py-2"><div class="max-h-[150px] overflow-y-auto p-2 text-justify">
+                                `+ listToShow[element].description + `
+                            </div></td>
+                            <td class="border px-4 py-2">${listToShow[element].adress}</td>
+                            <td class="border px-4 py-2">${parseFloat(listToShow[element].lat).toFixed(2) + ", " + parseFloat(listToShow[element].lon).toFixed(2)}</td>
+                            <td class="border px-4 py-2">${listToShow[element].price}</td>
+                            <td class="border px-4 py-2"><div class="border px-4 py-2">`;
+                                listToShow[element].imageLink.forEach(img => {
+                                    html += `<img src="` + img + `" class="rounded-lg duration-700 ease-in-out" data-carousel-item>`;
+                                })
+                html += `   </div></td>
+                            <td class="border px-4 py-2">
+                                <button type="button" id="edit-`+ element + `"
+                                    class="bg-teal-300 cursor-pointer rounded p-1 mx-1 text-white"><i class="fas fa-edit"></i></button>
                                 <button type="button" id="remove-`+ element + `"
-                                    class="font-medium text-red-600 dark:text-red-500 hover:underline">Remove</button>
-                                </td>
-                            </tr>`
+                                    class="bg-teal-300 cursor-pointer rounded p-1 mx-1 text-red-500"><i class="fas fa-trash"></i></button> 
+                            </td>
+                        </tr>`
             };
 
-            html += `
-                        </tbody>
-                    </table>
-                `;
+            html += `</tbody></table>`;
+
             parentElement.innerHTML = html;
 
             for (const key in listToShow) {
