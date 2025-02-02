4a7e544ef1dd1ab664437085ecdcb715e05d38f5
AngeloRipamonti <ripamontiangelo@itis-molinari.eu>
2025-02-02 10:50:56 +0100

    ErasmusPlus: start hometable

--- public/erasmus_plus/src/pages/milan/src/scripts/table.js
+++ public/erasmus_plus/src/pages/milan/src/scripts/table.js
@@ -7,31 +7,27 @@ export const createHomeTable = (parentElement, pubsub) => {
         render: async function () {
             if (!data) throw new Error("No data to render");
             let listToShow = data;
-            let html = `
-                    <table class="w-full text-sm text-left text-gray-500 dark:text-gray-400">
-                    <caption class="text-lg font-semibold text-left text-gray-900 dark:text-white p-4 sticky top-0"> List of all POI </caption>
-                        <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400 sticky top-0">
-                            <tr>
-                                <th scope="col" class="px-6 py-3 break-words whitespace-normal p-2">Title</th>
-                                <th scope="col" class="px-6 py-3 break-words whitespace-normal p-2">Address</th>
-                            </tr>
-                        </thead>
-                        <tbody>`;
+            let rows = 0;
+            let html = `<table class="table-fixed max-h-64 overflow-y-auto">
+						    <thead class="bg-gray-300 text-black px-2 py-3 border-solid border-gray-400 border-b">
+                                <tr>
+                                    <th class="border w-1/2 px-4 py-2">Title</th>
+                                    <th class="border w-1/2 px-4 py-2">Adress</th>
+                                </tr>
+							</thead>
+		                    <tbody style:"overflow: scroll">`;
 
             for (const element in listToShow) {
-                html += `<tr
-                                class="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600">
-                                <td class="px-6 py-4 break-words whitespace-normal p-2"><a href="#`+ listToShow[element].hash + `"
-                                        class="text-blue-600 dark:text-blue-400 hover:underline">`+ listToShow[element].name + `</a></td>
-                                <td class="px-6 py-4 break-words whitespace-normal p-2">`+ listToShow[element].adress + `</td>
-                            </tr>`
+                html += `<tr class="${(rows % 2 == 0) ? "bg-gray-200 text-black" : "bg-white text-black"}">
+                            <td class="border px-4 py-2"><a style="color: ${(rows % 2 == 0) ? 'blue' : '#2c5282'}" href="#${listToShow[element].hash}">${listToShow[element].name}</a></td>
+							<td class="border px-4 py-2">${listToShow[element].adress}</td>
+						</tr>`;
+                rows++;
             };
 
-            html += `
-                        </tbody>
-                    </table>
-                `;
-            //parentElement.innerHTML = html;
+            html += `</tbody></table>`;
+
+            parentElement.innerHTML = html;
         },
         renderFiltered: async function (filtered) {
             if (!data) throw new Error("No data to render");
@@ -67,9 +63,9 @@ export const createHomeTable = (parentElement, pubsub) => {
         build: async function (fetchC) {
             //pageCreator = pageC;
             fetchComponent = fetchC;
-            data = keySelector(((await fetchComponent.getData()).milan), ["name", "adress", "hash"]);
+            data = keySelector(((await fetchComponent.getData()).flensburg), ["name", "adress", "hash"]);
             pubsub.subscribe("changePOI", async () => {
-                data = keySelector(((await fetchComponent.getData()).milan), ["name", "adress", "hash"]);
+                data = keySelector(((await fetchComponent.getData()).flensburg), ["name", "adress", "hash"]);
                 await this.render();
             });
         }
@@ -142,7 +138,7 @@ export const createAdminTable = (parentElement, pubsub) => {
                 }
                 document.getElementById("edit-" + key).onclick = () => {
                     pubsub.publish("editPOI", [listToShow, key]);
-                    console.log("edit-" + key) ;
+                    console.log("edit-" + key);
                 }
             }
         },
