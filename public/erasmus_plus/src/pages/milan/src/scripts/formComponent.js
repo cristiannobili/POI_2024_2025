3e3e07750a7bc1e69503255d3a53a04e8ae05a4a
AngeloRipamonti <ripamontiangelo@itis-molinari.eu>
2025-02-04 18:42:20 +0100

    ErasmusPlus: fix modal form

--- public/erasmus_plus/src/pages/milan/src/scripts/formComponent.js
+++ public/erasmus_plus/src/pages/milan/src/scripts/formComponent.js
@@ -48,7 +48,7 @@ export const generateModalForm = (parentElement) => {
 
                     html += `
                     <div class="flex flex-wrap -mx-3 mb-6">
-                        <div class="w-full md:w-1/2 px-3 mb-6 md:mb-0">
+                        <div class="w-full px-3 mb-6 md:mb-0">
                             <label class="block uppercase tracking-wide text-gray-700 text-xs font-light mb-1"
                                 for="${e}">
                                 ${e}
@@ -62,7 +62,7 @@ export const generateModalForm = (parentElement) => {
                 } else {
                     html += `
                     <div class="flex flex-wrap -mx-3 mb-6">
-                        <div class="w-full md:w-1/2 px-3 mb-6 md:mb-0">
+                        <div class="w-full px-3 mb-6 md:mb-0">
                             <label class="block uppercase tracking-wide text-gray-700 text-xs font-light mb-1"
                                 for="${e}">
                                 ${e}
