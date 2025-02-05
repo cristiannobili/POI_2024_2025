f86e1f601fa9657ecd8e305c90d8b29dcd10fa88
Prostoiu Ciprian <prostoiuciprian@itis-molinari.eu>
2025-02-05 13:41:24 +0100

    login

--- public/secessione_americana/componenti/login.js
+++ public/secessione_americana/componenti/login.js
@@ -3,7 +3,7 @@ export const createLogin=()=>{
     return{
         login:(nome,password)=>{
             return new Promise((resolve, reject) => {
-                fetch("http://ws.cipiaceinfo.it/credential/login", { 
+                fetch("https://ws.cipiaceinfo.it/credential/login", { 
                   method: "POST",
                   headers: {
                      "content-type": "application/json",
