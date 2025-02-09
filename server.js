97d0d682605f3bacc022418f66afd085d97297cc
Cristian <c.nobili@gmail.com>
2025-02-09 11:12:25 +0100

    redirect

--- server.js
+++ server.js
@@ -8,9 +8,6 @@ app.use(bodyParser.urlencoded({
 }));
 const path = require('path');
 app.use("/poi", express.static(path.join(__dirname, "public")));
-app.get("/", (req, res) => {
-  res.redirect(301, "/poi");
-});
 const server = http.createServer(app);
 server.listen(5501, () => {
   console.log("- server running");
